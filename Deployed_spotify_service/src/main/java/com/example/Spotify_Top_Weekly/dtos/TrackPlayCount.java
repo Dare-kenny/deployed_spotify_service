feac8307bd4301ccfbc2ca3a77d9f4646d71cd00
I'm Akinrogunde Damilare <121644669+Dare-kenny@users.noreply.github.com>
2025-09-09 20:31:58 +0100

    Delete Deployed_spotify_service directory

--- Deployed_spotify_service/src/main/java/com/example/Spotify_Top_Weekly/dtos/TrackPlayCount.java
+++
@@ -1,8 +0,0 @@
-package com.example.Spotify_Top_Weekly.dtos;
-
-import lombok.Builder;
-
-@Builder
-public record TrackPlayCount(String trackId, String trackName, String artistName, long count) {
-
-}
