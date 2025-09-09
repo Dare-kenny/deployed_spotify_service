5fd9846fd14838484676bc03892075f445002caf
I'm Akinrogunde Damilare <121644669+Dare-kenny@users.noreply.github.com>
2025-09-09 19:28:58 +0100

    Add files via upload

---
+++ Deployed_spotify_service/src/test/java/com/example/Spotify_Top_Weekly/SpotifyTopWeeklyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spotify_Top_Weekly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyTopWeeklyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
